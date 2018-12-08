bff4b0334c0ff28edce500180ba3b61a43ff0c13
Paula Suarez <psuarez@dickeys2.local>
2018-12-08 08:02:39 -0600

    Adding updates to first part of node lab

--- server/server.js
+++ server/server.js
@@ -1,50 +1,42 @@
+//Part 1 of node lab assignment
+
+// building data path
 const path = require('path');
+
+// accessing the file system
 const fs = require('fs');
-const request = require('request');
 
+//create an array with chirp objects
+let chirps = {
+    chirp1: "beep",
+    chirp2: "beep, beep",
+    chirp3: "beep, beep, beep",
+    chirp4: "beep, beep, beep, beep",
+    chirp5: "beep, beep, beep, beep, beep"
+}
 
-//PART 1
-//let chirps = ["chirp1", "chirp2", "chirp3", "chirp4", "chirp5"];
+//establish data path to file where the array will wrtie to
+let dataPath = path.join(__dirname, '../chirps.json');
 
-//fs.writeFile("./chirps.json", chirps, (err) => {
-// if (err) {
-// console.error(err);
-// return;
-// };
-// console.log("File has been created");
-// console.log(chirps);
-//});
-
-//Part 2
-
-let dataPath = path.join(__dirname, '../reddit.js');
-
-request('https://www.reddit.com/r/popular.json', (err, res, body) => {
-
+//write chirps to the chirps.json file
+fs.writeFile(dataPath, JSON.stringify(chirps, null, 2), err => {
+    //output error message if error is found
     if (err) console.log(err);
+    //output message after file created and written to
+    console.log ("File has been created.")
 
-    console.log(res);
+})
+
+//read chirps.json file
+fs.readFile(dataPath, (err, data) => {
+    //out put error if error found when reading file
+    if (err) console.log(err);
+    //output data from file 
+    console.log(JSON.parse(data));
+})
 
 
-   //let articleInfo = [];
 
 
-    fs.writeFile(dataPath, res.body, err => {
-        if (err) console.log(err);
 
-        JSON.parse(body).data.children.forEach(item => {
-            console.log(item.data.title);
-           // console.log(item.data.author);
-            //console.log(item.data.url);
 
-    
-            //articleInfo.push(item.data.title);
-            //articleInfo.push(item.data.author);
-           // articleInfo.push(item.data.url);
-            //console.log(articleInfo);
-
-        });
-
-    });
-
-});
